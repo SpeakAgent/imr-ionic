765121bcb10b96ff7227005b31228de5023c4939
Fernando Jose <delgadof@seattleu.edu>
2016-08-03 12:01:27 -0700

    Fixing other stuff

--- www/js/templates/helpWords.js
+++ www/js/templates/helpWords.js
@@ -1,4 +1,4 @@
-mainApp.controller('HelpCtrl', function($scope, $cordovaMedia, $ionicLoading) {
+mainApp.controller('HelpCtrl', function($scope, $cordovaMedia, $ionicLoading, $http) {
   console.log("Help Words");
 
   var req = {
@@ -13,113 +13,12 @@ mainApp.controller('HelpCtrl', function($scope, $cordovaMedia, $ionicLoading) {
     $scope.user = data
   })
 
-  //Text to Speech
-  $scope.data = {
-    lost: 'I am lost.',
-    sick: 'I am sick.',
-    hurt: 'I am hurt.',
-    help: 'I need help.',
-    bathroom: 'Where is the bathroom?',
-    doThis: 'How can I do this?',
-    helpMe: 'Who can help me?',
-    nextStep: 'What is the next step?',
-    finished: 'When am I finished?'
-  };
-
   // make sure your the code gets executed only after `deviceready`.
   document.addEventListener('deviceready', function () {
     $scope.speak = function() {
       console.log("Yes");
       TTS.speak({
-        text: $scope.user,
-        locale: 'en-US',
-        rate: 1.5
-      }, function () {
-        console.log("SPEAK!!!");
-      });
-    }
-
-    $scope.speakSick = function() {
-      console.log("Yes");
-      TTS.speak({
-        text: $scope.data.sick,
-        locale: 'en-US',
-        rate: 1.5
-      }, function () {
-        console.log("SPEAK!!!");
-      });
-    }
-
-    $scope.speakHurt = function() {
-      console.log("Yes");
-      TTS.speak({
-        text: $scope.data.hurt,
-        locale: 'en-US',
-        rate: 1.5
-      }, function () {
-        console.log("SPEAK!!!");
-      });
-    }
-
-    $scope.speakHelp = function() {
-      console.log("Yes");
-      TTS.speak({
-        text: $scope.data.help,
-        locale: 'en-US',
-        rate: 1.5
-      }, function () {
-        console.log("SPEAK!!!");
-      });
-    }
-
-    $scope.speakBathroom = function() {
-      console.log("Yes");
-      TTS.speak({
-        text: $scope.data.bathroom,
-        locale: 'en-US',
-        rate: 1.5
-      }, function () {
-        console.log("SPEAK!!!");
-      });
-    }
-
-    $scope.speakDoThis = function() {
-      console.log("Yes");
-      TTS.speak({
-        text: $scope.data.doThis,
-        locale: 'en-US',
-        rate: 1.5
-      }, function () {
-        console.log("SPEAK!!!");
-      });
-    }
-
-    $scope.speakHelpMe = function() {
-      console.log("Yes");
-      TTS.speak({
-        text: $scope.data.helpMe,
-        locale: 'en-US',
-        rate: 1.5
-      }, function () {
-        console.log("SPEAK!!!");
-      });
-    }
-
-    $scope.speakNextStep = function() {
-      console.log("Yes");
-      TTS.speak({
-        text: $scope.data.nextStep,
-        locale: 'en-US',
-        rate: 1.5
-      }, function () {
-        console.log("SPEAK!!!");
-      });
-    }
-
-    $scope.speakFinished = function() {
-      console.log("Yes");
-      TTS.speak({
-        text: $scope.data.finished,
+        text: "I am hurt",
         locale: 'en-US',
         rate: 1.5
       }, function () {
